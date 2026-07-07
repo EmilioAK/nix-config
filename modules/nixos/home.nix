93255a333d0365840d40f113d23ed2c9e327ae2d
EmilioAK <me@emilioak.dev>
2026-07-07 22:15:50 +0200

    remove tmux alerts

--- modules/nixos/home.nix
+++ modules/nixos/home.nix
@@ -4,17 +4,4 @@ let
   dotfile = path: config.lib.file.mkOutOfStoreSymlink "${flakeRoot}/dotfiles/${path}";
 in {
   home.file.".tmux.conf".source = dotfile "tmux/tmux.conf";
-
-  home.file.".local/bin/tmux-bell-alerts" = {
-    source = dotfile "tmux/bin/tmux-bell-alerts";
-    executable = true;
-  };
-  home.file.".local/bin/tmux-clear-pi-agent-alert" = {
-    source = dotfile "tmux/bin/tmux-clear-pi-agent-alert";
-    executable = true;
-  };
-  home.file.".local/bin/tmux-jump-agent-alert" = {
-    source = dotfile "tmux/bin/tmux-jump-agent-alert";
-    executable = true;
-  };
 }
